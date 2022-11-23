677e8313ee3d452b13b6da3332db3afd42c8b5a4
Emil Kudla <65461069+SprzedawcaKoz@users.noreply.github.com>
2022-11-24 00:56:20 +0100

    exercises 1 and 2

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('smash or pass?');
+  
   return (
     <View style={styles.container}>
       <Text>I live in your walls</Text>
